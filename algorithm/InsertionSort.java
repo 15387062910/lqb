5f46778a86d870a0d30d8334a9ea9af3b01d88b0
WANG YIBING <1572834916@qq.com>
2018-11-25 22:47:26 +0800

    排序

--- algorithm/InsertionSort.java
+++ algorithm/InsertionSort.java
@@ -7,10 +7,9 @@ public class InsertionSort {
 			return;
 		}
 		for (int i = 1; i < arr.length; i++) {
-			for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j++) {
+			for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
 				swap(arr, j, j+1);
 			}
-			
 		}
 	}
 
