06e5312e0865a4877940ebbff12ed1a7324ef202
WANG YIBING <1572834916@qq.com>
2018-11-26 20:50:02 +0800

    排序

--- algorithm/InsertionSort.java
+++ algorithm/InsertionSort.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 	public static void insertionSort(int[] arr) {
 		if (arr == null || arr.length < 2) {
@@ -16,5 +18,94 @@ public class InsertionSort {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
+	}
+	
+	// for test
+	public static void comparator(int[] arr) {
+		// call java built-in sort
+		Arrays.sort(arr);
+	}
+
+	// for test
+	public static int[] generateRandomArray(int maxSize, int maxValue) {
+		// generate a random array
+		// (int) ((maxSize + 1) * Math.random()) -> [0, maxSize]
+		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];		
+		for (int i = 0; i < arr.length; i++) {
+			// value of array element -> [1 - maxValue, maxValue]
+			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+		}
+		return arr;
+	}
+
+	// for test
+	public static int[] copyArray(int[] arr) {
+		// copy a array to another array
+		if (arr == null) {
+			return null;
+		}
+		int[] res = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			res[i] = arr[i];
+		}
+		return res;
+	}
+
+	// for test
+	public static boolean isEqual(int[] arr1, int[] arr2) {
+		// to judge two array is equal
+		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+			return false;
+		}
+		if (arr1 == null && arr2 == null) {
+			return true;
+		}
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// for test
+	public static void printArray(int[] arr) {
+		// print all elements of array 
+		if (arr == null) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	// for test
+	public static void main(String[] args) {
+		// test main function
+		int testTime = 500000;
+		int maxSize = 100;
+		int maxValue = 100;
+		boolean succeed = true;
+		for (int i = 0; i < testTime; i++) {
+			int[] arr1 = generateRandomArray(maxSize, maxValue);
+			int[] arr2 = copyArray(arr1);
+			insertionSort(arr1);
+			comparator(arr2);
+			if (!isEqual(arr1, arr2)) {
+				succeed = false;
+				break;
+			}
+		}
+		System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+		
+		// to generate a array and bubbleSort it
+		int[] arr = generateRandomArray(maxSize, maxValue);
+		printArray(arr);
+		insertionSort(arr);
+		printArray(arr);
 	}
 }
