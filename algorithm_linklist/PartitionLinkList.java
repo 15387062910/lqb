fb34ecce53fe14b4214eeb8ff2f37b1429ade97e
WANG YIBING <1572834916@qq.com>
2018-12-06 00:20:34 +0800

    完成PartitionLinkList

--- algorithm_linklist/PartitionLinkList.java
+++ algorithm_linklist/PartitionLinkList.java
@@ -24,7 +24,7 @@ public class PartitionLinkList {
 
 	// convert the linklist to array
 	public static int[] LinkListToArray(Node head) {
-		if(head == null){
+		if (head == null) {
 			return null;
 		}
 		int length = getLinkListLength(head);
@@ -49,8 +49,36 @@ public class PartitionLinkList {
 			n.next = new Node(arr[i]);
 			n = n.next;
 		}
-		
+
 		return head;
+	}
+
+	// 按顺序合并两个链表(每个都可能为null)
+	public static Node mergeLinkList(Node head1, Node head2) {
+		if (head1 == null && head2 == null) {
+			return null;
+		} else if (head1 == null) {
+			return head2;
+		} else if (head2 == null) {
+			return head1;
+		} else {
+			Node head = head1;
+			Node tail = head;
+			Node tail_a = head1.next;
+			while(tail_a != null){
+				tail.next = tail_a;
+				tail = tail_a;
+				tail_a = tail_a.next;
+			}
+			Node tail_b = head2;
+			while(tail_b != null){
+				tail.next = tail_b;
+				tail = tail_b;
+				tail_b = tail_b.next;
+			}
+			
+			return head;
+		}
 	}
 
 	// for test
@@ -80,10 +108,11 @@ public class PartitionLinkList {
 		arr[j] = temp;
 	}
 
+	// partition a array
 	public static void partition(int[] arr, int num) {
 		int less = -1;
 		int more = arr.length;
-		for (int i = 0; i < arr.length && i <= more; i++) {
+		for (int i = 0; i < more; i++) {
 			if (arr[i] < num) {
 				swap(arr, i, ++less);
 			} else if (arr[i] > num) {
@@ -98,15 +127,69 @@ public class PartitionLinkList {
 		int[] arr = LinkListToArray(head);
 		partition(arr, num);
 		head = ArrayToLinkList(arr);
-		
+
 		return head;
 	}
 
 	// 进阶方法
 	public static Node partitionLinkList2(Node head, int num) {
+		Node sH = null; // small head
+		Node sT = null; // small tail
+		Node eH = null; // equal head
+		Node eT = null; // equal tail
+		Node bH = null; // big head
+		Node bT = null; // big tail
+		Node next = null; // save next node
+		while (head != null) {
+			// 拿出头节点head
+			next = head.next;
+			head.next = null;
+			// 根据head的值将head放入对应的链表中
+			if (head.value < num) {
+				if (sH == null) {
+					sH = head;
+					sT = head;
+				} else {
+					sT.next = head;
+					sT = head;
+				}
+			} else if (head.value > num) {
+				if (bH == null) {
+					bH = head;
+					bT = head;
+				} else {
+					bT.next = head;
+					bT = head;
+				}
+			} else {
+				if (eH == null) {
+					eH = head;
+					eT = head;
+				} else {
+					eT.next = head;
+					eT = head;
+				}
+			}
 
-		Node res = new Node(0);
-		return res;
+			// 拿到原head之后的链表
+			head = next;
+		}
+
+		// printLinkedList(sH);
+		// printLinkedList(eH);
+		// printLinkedList(bH);
+
+		// 将三个链表合并在一起
+// 注释的是左神的写法:
+//		if (sT != null) {
+//			sT.next = eH;
+//			eT = eT == null ? sT : eT;
+//		}
+//		if (eT != null) {
+//			eT.next = bH;
+//		}
+//		return sH != null ? sH : (eH != null ? eH : bH);
+		return mergeLinkList(mergeLinkList(sH, eH), bH);
 	}
 
 	// test
@@ -119,9 +202,20 @@ public class PartitionLinkList {
 		head.next.next.next.next = new Node(8);
 		head.next.next.next.next.next = new Node(1);
 		printLinkedList(head);
-		head = partitionLinkList1(head, 5);		// partition linklist
+		head = partitionLinkList1(head, 5); // partition linklist
 		printLinkedList(head);
+		System.out.println("============================");
 
+		// test partitionLinkList2
+		Node head2 = new Node(1);
+		head2.next = new Node(3);
+		head2.next.next = new Node(6);
+		head2.next.next.next = new Node(5);
+		head2.next.next.next.next = new Node(8);
+		head2.next.next.next.next.next = new Node(1);
+		printLinkedList(head2);
+		head2 = partitionLinkList2(head2, 5);
+		printLinkedList(head2);
 	}
 
 }
